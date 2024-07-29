869bd6463343d26928b859347ba24b10f194e818
rubyroy21 <rubyroy722@gmail.com>
2024-07-29 23:52:06 +0530

    first commit

---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div>
+      <p>Hey there,</p>
+      <p>Wonderful soul! 🌺 We’re sending you a warm hug through this note.</p>
+    </div>
+  );
+};
+
+export default Message;
---
+++ src/utils/qnaData.js
@@ -0,0 +1,18 @@
+const qnaData = [
+  {
+    question: "Tell me about yourself",
+    type: "textArea",
+  },
+  {
+    question: "Have you opened the envelope and read the note?",
+    a: "Yes",
+    b: "No",
+    type: "text",
+  },
+  {
+    question: "Do you have something to say 👉👈 uwu?",
+    type: "textArea",
+  },
+];
+
+export default qnaData;
